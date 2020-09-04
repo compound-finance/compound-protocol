b083de6b3b9b5fbc8446adb40200db396b2330dc
Aryeh Greenberg <aryeh@aryehgreenberg.com>
2020-09-04 17:57:03 -0400

    Lint fix

--- contracts/JumpRateModelV2.sol
+++ contracts/JumpRateModelV2.sol
@@ -10,9 +10,18 @@ import "./InterestRateModel.sol";
   * @notice Supports only for V2 cTokens
   */
 contract JumpRateModelV2 is InterestRateModel, BaseJumpRateModelV2  {
+	
+	/**
+     * @notice Calculates the current borrow rate per block, with the error code expected by the market
+     * @param cash The amount of cash in the market
+     * @param borrows The amount of borrows in the market
+     * @param reserves The amount of reserves in the market
+     * @return The borrow rate percentage per block as a mantissa (scaled by 1e18)
+     */
     function getBorrowRate(uint cash, uint borrows, uint reserves) external view returns (uint) {
         return getBorrowRateInternal(cash, borrows, reserves);
     }
 
-    constructor(uint baseRatePerYear, uint multiplierPerYear, uint jumpMultiplierPerYear, uint kink_, address owner_) BaseJumpRateModelV2(baseRatePerYear,multiplierPerYear,jumpMultiplierPerYear,kink_,owner_) public {}
+    constructor(uint baseRatePerYear, uint multiplierPerYear, uint jumpMultiplierPerYear, uint kink_, address owner_) 
+    	BaseJumpRateModelV2(baseRatePerYear,multiplierPerYear,jumpMultiplierPerYear,kink_,owner_) public {}
 }
--- contracts/LegacyJumpRateModelV2.sol
+++ contracts/LegacyJumpRateModelV2.sol
@@ -10,8 +10,18 @@ import "./LegacyInterestRateModel.sol";
   * @notice Supports only legacy cTokens
   */
 contract LegacyJumpRateModelV2 is LegacyInterestRateModel, BaseJumpRateModelV2  {
+
+	/**
+     * @notice Calculates the current borrow rate per block, with the error code expected by the market
+     * @param cash The amount of cash in the market
+     * @param borrows The amount of borrows in the market
+     * @param reserves The amount of reserves in the market
+     * @return (Error, The borrow rate percentage per block as a mantissa (scaled by 1e18))
+     */
     function getBorrowRate(uint cash, uint borrows, uint reserves) external view returns (uint, uint) {
         return (0,getBorrowRateInternal(cash, borrows, reserves));
     }
-    constructor(uint baseRatePerYear, uint multiplierPerYear, uint jumpMultiplierPerYear, uint kink_, address owner_) BaseJumpRateModelV2(baseRatePerYear,multiplierPerYear,jumpMultiplierPerYear,kink_,owner_) public {}
+    
+    constructor(uint baseRatePerYear, uint multiplierPerYear, uint jumpMultiplierPerYear, uint kink_, address owner_) 
+    	BaseJumpRateModelV2(baseRatePerYear,multiplierPerYear,jumpMultiplierPerYear,kink_,owner_) public {}
 }
