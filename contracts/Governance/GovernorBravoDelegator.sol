3de54ce8a698ac59c37895b11208dcf334919335
Aryeh Greenberg <13561405+arr00@users.noreply.github.com>
2020-12-20 22:56:43 -0500

    proxy first run

---
+++ contracts/Governance/GovernorBravoDelegator.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.5.16;
+
+import "./GovernorBravoInterfaces.sol";
+
+contract GovernorBravoDelegator is GovernorBravoStorageV1 {
+	constructor(
+			address timelock_,
+			address comp_,
+			address admin_,
+	        address implementation_,
+	        uint256 votingPeriod_,
+	        uint256 votingDelay_) public {
+		admin = msg.sender;
+
+
+		// First delegate gets to initialize the delegator (i.e. storage contract)
+        delegateTo(implementation_, abi.encodeWithSignature("initialize(address,address,uint256,uint256)",
+                                                            timelock_,
+                                                            comp_,
+                                                            votingPeriod_,
+                                                            votingDelay_));
+
+        _setImplementation(implementation_);
+
+		admin = admin_;
+	}
+
+
+	/**
+     * @notice Called by the admin to update the implementation of the delegator
+     * @param implementation_ The address of the new implementation for delegation
+     */
+    function _setImplementation(address implementation_) public {
+        require(msg.sender == admin, "GovernorDelegator::_setImplementation: Caller must be admin");
+
+        address oldImplementation = implementation;
+        implementation = implementation_;
+
+        emit NewImplementation(oldImplementation, implementation);
+    }
+
+    /**
+     * @notice Internal method to delegate execution to another contract
+     * @dev It returns to the external caller whatever the implementation returns or forwards reverts
+     * @param callee The contract to delegatecall
+     * @param data The raw data to delegatecall
+     * @return The returned bytes from the delegatecall
+     */
+    function delegateTo(address callee, bytes memory data) internal returns (bytes memory) {
+        (bool success, bytes memory returnData) = callee.delegatecall(data);
+        assembly {
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize)
+            }
+        }
+        return returnData;
+    }
+
+	/**
+     * @dev Delegates execution to an implementation contract.
+     * It returns to the external caller whatever the implementation returns
+     * or forwards reverts.
+     */
+    function () payable external {
+    	require(msg.value == 0,"GovernorDelegator:fallback: cannot send value to fallback");
+
+        // delegate all other functions to current implementation
+        (bool success, ) = implementation.delegatecall(msg.data);
+
+        assembly {
+              let free_mem_ptr := mload(0x40)
+              returndatacopy(free_mem_ptr, 0, returndatasize)
+
+              switch success
+              case 0 { revert(free_mem_ptr, returndatasize) }
+              default { return(free_mem_ptr, returndatasize) }
+        }
+    }
+}
