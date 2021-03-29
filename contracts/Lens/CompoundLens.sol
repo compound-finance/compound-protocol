53c19958ea1694cfdd969b6106fa804542b65469
Torrey Atcitty <torrey@compound.finance>
2021-03-29 16:42:20 -0700

    Add in gov bravo versions or proposal and vote receition lens functions.

--- contracts/Lens/CompoundLens.sol
+++ contracts/Lens/CompoundLens.sol
@@ -186,6 +186,28 @@ contract CompoundLens {
         return res;
     }
 
+    struct GovBravoReceipt {
+        uint proposalId;
+        bool hasVoted;
+        uint8 support;
+        uint96 votes;
+    }
+
+    function getGovBravoReceipts(GovernorBravo governor, address voter, uint[] memory proposalIds) public view returns (GovBravoReceipt[] memory) {
+        uint proposalCount = proposalIds.length;
+        GovReceipt[] memory res = new GovReceipt[](proposalCount);
+        for (uint i = 0; i < proposalCount; i++) {
+            GovernorBravo.Receipt memory receipt = governor.getReceipt(proposalIds[i], voter);
+            res[i] = GovReceipt({
+                proposalId: proposalIds[i],
+                hasVoted: receipt.hasVoted,
+                support: receipt.support,
+                votes: receipt.votes
+            });
+        }
+        return res;
+    }
+
     struct GovProposal {
         uint proposalId;
         address proposer;
@@ -250,6 +272,78 @@ contract CompoundLens {
                 executed: false
             });
             setProposal(res[i], governor, proposalIds[i]);
+        }
+        return res;
+    }
+
+    struct GovBravoProposal {
+        uint proposalId;
+        address proposer;
+        uint eta;
+        address[] targets;
+        uint[] values;
+        string[] signatures;
+        bytes[] calldatas;
+        uint startBlock;
+        uint endBlock;
+        uint forVotes;
+        uint againstVotes;
+        uint abstainVotes;
+        bool canceled;
+        bool executed;
+    }
+
+    function setBravoProposal(GovBravoProposal memory res, GovernorBravo governor, uint proposalId) internal view {
+        (
+            ,
+            address proposer,
+            uint eta,
+            uint startBlock,
+            uint endBlock,
+            uint forVotes,
+            uint againstVotes,
+            uint abstainVotes,
+            bool canceled,
+            bool executed
+        ) = governor.proposals(proposalId);
+        res.proposalId = proposalId;
+        res.proposer = proposer;
+        res.eta = eta;
+        res.startBlock = startBlock;
+        res.endBlock = endBlock;
+        res.forVotes = forVotes;
+        res.againstVotes = againstVotes;
+        res.abstainVotes = abstainVotes
+        res.canceled = canceled;
+        res.executed = executed;
+    }
+
+    function getGovBravoProposals(GovernorBravo governor, uint[] calldata proposalIds) external view returns (GovBravoProposal[] memory) {
+        GovProposal[] memory res = new GovProposal[](proposalIds.length);
+        for (uint i = 0; i < proposalIds.length; i++) {
+            (
+                address[] memory targets,
+                uint[] memory values,
+                string[] memory signatures,
+                bytes[] memory calldatas
+            ) = governor.getActions(proposalIds[i]);
+            res[i] = GovProposal({
+                proposalId: 0,
+                proposer: address(0),
+                eta: 0,
+                targets: targets,
+                values: values,
+                signatures: signatures,
+                calldatas: calldatas,
+                startBlock: 0,
+                endBlock: 0,
+                forVotes: 0,
+                againstVotes: 0,
+                abstainVotes: 0,
+                canceled: false,
+                executed: false
+            });
+            setBravoProposal(res[i], governor, proposalIds[i]);
         }
         return res;
     }
