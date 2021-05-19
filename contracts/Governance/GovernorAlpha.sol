1901105531eb5bcc108e4736fe7b92e74b36ac75
Owocki <ksowocki@gmail.com>
2021-05-18 20:40:23 -0600

    a bit closer

--- contracts/Governance/GovernorAlpha.sol
+++ contracts/Governance/GovernorAlpha.sol
@@ -329,3 +329,50 @@ interface TimelockInterface {
 interface CompInterface {
     function getPriorVotes(address account, uint blockNumber) external view returns (uint96);
 }
+
+contract QuadraticGovernorAlpha is GovernorAlpha {
+    /// @notice The name of this contract
+    string public constant name = "Quadratic Governor Alpha";
+
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function _castVote(address voter, uint proposalId, bool support) internal {
+        /* insert POH code */
+        IProofOfHumanity POH = IProofOfHumanity(0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb);
+        require (POH.isRegistered(voter), "Voter must be registered on POH");
+        /* end POH code */
+
+        require(state(proposalId) == ProposalState.Active, "GovernorAlpha::_castVote: voting is closed");
+        Proposal storage proposal = proposals[proposalId];
+        Receipt storage receipt = proposal.receipts[voter];
+        require(receipt.hasVoted == false, "GovernorAlpha::_castVote: voter already voted");
+        uint96 votes = comp.getPriorVotes(voter, proposal.startBlock);
+        /* insert quadratic votes */
+        votes = sqrt(votes);
+        /* end quadratic votes */
+
+        if (support) {
+            proposal.forVotes = add256(proposal.forVotes, votes);
+        } else {
+            proposal.againstVotes = add256(proposal.againstVotes, votes);
+        }
+
+        receipt.hasVoted = true;
+        receipt.support = support;
+        receipt.votes = votes;
+
+        emit VoteCast(voter, proposalId, support, votes);
+    }
+
+}
