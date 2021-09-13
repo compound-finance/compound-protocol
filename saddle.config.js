ea8dfd19321d63d18b9150d20333a80de2c47fe4
Yos Riady <yosriady@gmail.com>
2021-09-13 18:28:33 +0800

    Deploy cpor on mainnet, rinkeby

--- saddle.config.js
+++ saddle.config.js
@@ -144,7 +144,7 @@ module.exports = {
         ],
         gas_price: [
           {env: "GAS_PRICE"},
-          {default: "12000000000"}
+          {default: "1000000000"}
         ],
         options: {
           transactionConfirmationBlocks: 1,
