f1139c9e4e581f22a9957e1257acdcc01bad5901
davidlucid <davidlucidsl@gmail.com>
2021-09-13 13:27:06 -0500

    Fix CDelegateInterface._setImplementationSafe

--- contracts/CTokenInterfaces.sol
+++ contracts/CTokenInterfaces.sol
@@ -331,7 +331,7 @@ contract CDelegateInterface is CDelegationStorage {
      * @param allowResign Flag to indicate whether to call _resignImplementation on the old implementation
      * @param becomeImplementationData The encoded bytes data to be passed to _becomeImplementation
      */
-    function _setImplementation(address implementation_, bool allowResign, bytes memory becomeImplementationData) public;
+    function _setImplementationSafe(address implementation_, bool allowResign, bytes calldata becomeImplementationData) external;
 
     /**
      * @notice Called by the delegator on a delegate to initialize it for duty
