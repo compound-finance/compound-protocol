67f40b05f37dde1fa890e554820587009904ec6c
Your Name <you@example.com>
2022-01-31 16:17:46 -0800

    add interfaces

---
+++ contracts/Interface/BasePriceOracle.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.12;
+
+import "./PriceOracle.sol";
+
+/**
+ * @title BasePriceOracle
+ * @notice Returns prices of underlying tokens directly without the caller having to specify a cToken address.
+ * @dev Implements the `PriceOracle` interface.
+ * @author David Lucid <david@rari.capital> (https://github.com/davidlucid)
+ */
+interface BasePriceOracle is PriceOracle {
+    /**
+     * @notice Get the price of an underlying asset.
+     * @param underlying The underlying asset to get the price of.
+     * @return The underlying asset price in ETH as a mantissa (scaled by 1e18).
+     * Zero means the price is unavailable.
+     */
+    function price(address underlying) external view returns (uint);
+}
