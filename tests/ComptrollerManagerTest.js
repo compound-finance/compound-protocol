47f1ae1986a525b3901b3ff225731a5769392fbf
Nickolas <101463700+0xnickolas@users.noreply.github.com>
2022-03-21 13:51:00 +0000

    ComptrollerManager can claim airdrop (#1)

--- tests/ComptrollerManagerTest.js
+++ tests/ComptrollerManagerTest.js
@@ -1,41 +1,95 @@
 const {
   makeComptroller,
-} = require('./Utils/Compound');
+} = require("./Utils/Compound");
 const {
   etherExp,
   etherUnsigned,
-} = require('./Utils/Ethereum');
+  solidityKeccak256,
+} = require("./Utils/Ethereum");
 
 async function compAccrued(comptroller, user) {
-  return etherUnsigned(await call(comptroller, 'compAccrued', [user]));
+  return etherUnsigned(await call(comptroller, "compAccrued", [user]));
 }
 
 async function compBalance(comp, user) {
-  return etherUnsigned(await call(comp, 'balanceOf', [user]));
+  return etherUnsigned(await call(comp, "balanceOf", [user]));
 }
 
-describe('ComptrollerManager', () => {
+describe("ComptrollerManager", () => {
   let root, holder;
   let comptrollerManager, comp, comptroller1, comptroller2;
   beforeEach(async () => {
     [root, holder] = saddle.accounts;
-    comptrollerManager = await deploy('ComptrollerManager');
-    comp = await deploy('Comp', [root, 'COMP', 'Compound']);
-    comptroller1 = await makeComptroller({comp});
-    comptroller2 = await makeComptroller({comp});
+    comptrollerManager = await deploy("ComptrollerManager");
+    comp = await deploy("Comp", [root, "COMP", "Compound"]);
+    comptroller1 = await makeComptroller({ comp });
+    comptroller2 = await makeComptroller({ comp });
   });
 
-  describe('claimComp', () => {
-    it('should claim from multiple Comptroller', async () => {
-      const compRemaining = etherExp(1), accruedAmt = etherUnsigned(0.0009e18);
-      await send(comptroller1.comp, 'transfer', [comptroller1._address, compRemaining], {from: root});
-      await send(comptroller1, 'setCompAccrued', [holder, accruedAmt]);
-      await send(comptroller2.comp, 'transfer', [comptroller2._address, compRemaining], {from: root});
-      await send(comptroller2, 'setCompAccrued', [holder, accruedAmt]);
-      await send(comptrollerManager, 'claimComp', [holder, [comptroller1._address, comptroller2._address]]);
+  describe("claimComp", () => {
+    it("should claim from multiple Comptroller", async () => {
+      const compRemaining = etherExp(1),
+        accruedAmt = etherUnsigned(0.0009e18);
+      await send(
+        comptroller1.comp,
+        "transfer",
+        [comptroller1._address, compRemaining],
+        { from: root }
+      );
+      await send(comptroller1, "setCompAccrued", [holder, accruedAmt]);
+      await send(
+        comptroller2.comp,
+        "transfer",
+        [comptroller2._address, compRemaining],
+        { from: root }
+      );
+      await send(comptroller2, "setCompAccrued", [holder, accruedAmt]);
+      await send(comptrollerManager, "claimComp", [
+        [comptroller1._address, comptroller2._address],
+        holder,
+      ]);
       expect(await compAccrued(comptroller1, holder)).toEqualNumber(0);
       expect(await compAccrued(comptroller2, holder)).toEqualNumber(0);
-      expect(await compBalance(comp, holder)).toEqualNumber(accruedAmt.times(2));
+      expect(await compBalance(comp, holder)).toEqualNumber(
+        accruedAmt.times(2)
+      );
+    });
+
+    it("should claim airdrop", async () => {
+      const merkleRoot = solidityKeccak256(
+        ["address", "uint256"],
+        [holder, etherExp(0.001).toString()]
+      );
+      await send(comptroller1, "_setAirdrop", [merkleRoot, 100, 200]);
+      await send(comptroller1, "setBlockNumber", [150]);
+
+      const compRemaining = etherExp(1),
+        accruedAmt = etherUnsigned(0.0009e18);
+      await send(
+        comptroller1.comp,
+        "transfer",
+        [comptroller1._address, compRemaining],
+        { from: root }
+      );
+      await send(comptroller1, "setCompAccrued", [holder, accruedAmt]);
+      await send(
+        comptroller2.comp,
+        "transfer",
+        [comptroller2._address, compRemaining],
+        { from: root }
+      );
+      await send(comptroller2, "setCompAccrued", [holder, accruedAmt]);
+      await send(comptrollerManager, "claimComp", [
+        [comptroller1._address, comptroller2._address],
+        holder,
+        etherExp(0.001),
+        [],
+      ]);
+      expect(await compAccrued(comptroller1, holder)).toEqualNumber(0);
+      expect(await compAccrued(comptroller2, holder)).toEqualNumber(0);
+      expect(await compBalance(comp, holder)).toEqualNumber(
+        accruedAmt.times(2).plus(etherExp(0.001).multipliedBy(50))
+      );
     });
   });
 });
