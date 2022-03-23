442519e77e42939b0d5cca2e811cd313b43ce4be
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2022-03-23 18:20:22 -0400

    Fix L07: Outdated Solidity versions

--- contracts/ComptrollerInterface.sol
+++ contracts/ComptrollerInterface.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.10;
 
 abstract contract ComptrollerInterface {
     /// @notice Indicator that this is a Comptroller contract (for inspection)
