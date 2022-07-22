d2bc474624406957bf65f3657e145a21531fc740
arr00 <13561405+arr00@users.noreply.github.com>
2022-07-22 18:54:20 +0200

    add post deploy simulation

--- contracts/Governance/GovernorBravoDelegateG1.sol
+++ contracts/Governance/GovernorBravoDelegateG1.sol
@@ -3,7 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "./GovernorBravoInterfaces.sol";
 
-contract GovernorBravoDelegate is GovernorBravoDelegateStorageV1, GovernorBravoEvents {
+contract GovernorBravoDelegateG1 is GovernorBravoDelegateStorageV1, GovernorBravoEvents {
 
     /// @notice The name of this contract
     string public constant name = "Compound Governor Bravo";
