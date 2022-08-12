0d884ab581d9e0ee1697608c72776884fda61051
bjkanbour <bjkanbour@gmail.com>
2022-08-12 12:57:53 -0600

    update transfer function for glp token

--- contracts/CErc20.sol
+++ contracts/CErc20.sol
@@ -165,7 +165,11 @@ contract CErc20 is CToken, CErc20Interface {
         address underlying_ = underlying;
         EIP20NonStandardInterface token = EIP20NonStandardInterface(underlying_);
         uint balanceBefore = EIP20Interface(underlying_).balanceOf(address(this));
-        token.transferFrom(from, address(this), amount);
+        if(isGLP){
+            stakedGLP.transferFrom(from, address(this), amount);
+        } else {
+            token.transferFrom(from, address(this), amount);
+        }
 
         bool success;
         assembly {
