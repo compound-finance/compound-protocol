493c1258a95a10dd9812b7da5227b9863f8e39a1
frehml <frederic.hamelink@hotmail.com>
2022-10-07 19:22:22 +0200

    add circle ci support

--- contracts/TransferHelper.sol
+++ contracts/TransferHelper.sol
@@ -16,9 +16,18 @@ library TransferHelper {
         address to,
         uint256 value
     ) internal {
-        (bool success, bytes memory data) =
-            token.call(abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'STF');
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(
+                IERC20.transferFrom.selector,
+                from,
+                to,
+                value
+            )
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "STF"
+        );
     }
 
     /// @notice Transfers tokens from msg.sender to a recipient
@@ -31,8 +40,13 @@ library TransferHelper {
         address to,
         uint256 value
     ) internal {
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.transfer.selector, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'ST');
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(IERC20.transfer.selector, to, value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "ST"
+        );
     }
 
     /// @notice Approves the stipulated contract to spend the given allowance in the given token
@@ -45,8 +59,13 @@ library TransferHelper {
         address to,
         uint256 value
     ) internal {
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.approve.selector, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'SA');
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(IERC20.approve.selector, to, value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "SA"
+        );
     }
 
     /// @notice Transfers ETH to the recipient address
@@ -55,6 +74,6 @@ library TransferHelper {
     /// @param value The value to be transferred
     function safeTransferETH(address to, uint256 value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
-        require(success, 'STE');
+        require(success, "STE");
     }
 }
