c11b8a4561890fa9a5c9e665296d8beb9f333a42
frehml <frederic.hamelink@hotmail.com>
2022-11-07 22:36:15 +0100

    fix address bug

--- tender/test/Liquidations.ts
+++ tender/test/Liquidations.ts
@@ -1,44 +1,83 @@
-import { JsonRpcSigner, JsonRpcProvider, ExternalProvider, } from "@ethersproject/providers";
-import "@nomiclabs/hardhat-ethers"
+import {
+  JsonRpcSigner,
+  JsonRpcProvider,
+  ExternalProvider,
+} from "@ethersproject/providers";
+import "@nomiclabs/hardhat-ethers";
 import hre, { artifacts, ethers } from "hardhat";
-import { getAbiFromArbiscan, resetNetwork, parseAbiFromJson, getDeployments} from "./utils/TestUtil";
+import {
+  getAbiFromArbiscan,
+  resetNetwork,
+  parseAbiFromJson,
+  getDeployments,
+} from "./utils/TestUtil";
 import { Contract, BigNumber } from "ethers";
 import { formatAmount } from "./utils/TokenUtil";
-import { resolve } from 'path'
+import { resolve } from "path";
 import { copyFile, readFileSync } from "fs";
 import { deploy } from "./deploy/cdelegators";
-import { WHALES, CTOKENS, IERC20, CERC20 } from './utils/constants';
+import { WHALES, CTOKENS, IERC20, CERC20 } from "./utils/constants";
 import { expect } from "./utils/chai";
 
 const provider = hre.network.provider;
 
 const borrowedBalance = async (tTokenContract, wallet) => {
-  const tTokenBalance = await tTokenContract.connect(wallet).borrowBalanceStored(wallet.address);
-  const exchangeRate = await tTokenContract.connect(wallet).exchangeRateStored();
-  return tTokenBalance.mul(exchangeRate).div(formatAmount('1', 18))
+  const tTokenBalance = await tTokenContract
+    .connect(wallet)
+    .borrowBalanceStored(wallet.address);
+  const exchangeRate = await tTokenContract
+    .connect(wallet)
+    .exchangeRateStored();
+  return tTokenBalance.mul(exchangeRate).div(formatAmount("1", 18));
 };
 
 const underlyingBalance = async (tTokenContract, wallet) => {
-  const tTokenBalance = await tTokenContract.connect(wallet).balanceOf(wallet.address);
-  const exchangeRate = await tTokenContract.connect(wallet).exchangeRateStored();
+  const tTokenBalance = await tTokenContract
+    .connect(wallet)
+    .balanceOf(wallet.address);
+  const exchangeRate = await tTokenContract
+    .connect(wallet)
+    .exchangeRateStored();
   // amount of underlying tokens minus interest accrued
-  return tTokenBalance.mul(exchangeRate).div(formatAmount('1', 18))
+  return tTokenBalance.mul(exchangeRate).div(formatAmount("1", 18));
 };
 
-const preLiquidationTest = async (deployments, collateralToken, borrowToken) => {
-
-  const uCollateralWallet = await ethers.getImpersonatedSigner(WHALES[collateralToken.underlyingSymbol]);
-  const uBorrowWallet = await ethers.getImpersonatedSigner(WHALES[borrowToken.underlyingSymbol]);
+const preLiquidationTest = async (
+  deployments,
+  collateralToken,
+  borrowToken
+) => {
+  const uCollateralWallet = await ethers.getImpersonatedSigner(
+    WHALES[collateralToken.underlyingSymbol]
+  );
+  const uBorrowWallet = await ethers.getImpersonatedSigner(
+    WHALES[borrowToken.underlyingSymbol]
+  );
 
   await fundWithEth(uCollateralWallet.address);
   await fundWithEth(uCollateralWallet.address);
 
-  const uTokenCollateral = await ethers.getContractAt(IERC20, collateralToken.underlying)
-  const tTokenCollateral = await ethers.getContractAt(CERC20, collateralToken.address)
-  const uTokenBorrow = await ethers.getContractAt(IERC20, borrowToken.underlying)
-  const tTokenBorrow = await ethers.getContractAt(CERC20, borrowToken.address)
+  const uTokenCollateral = await ethers.getContractAt(
+    IERC20,
+    collateralToken.underlying
+  );
 
-  const comptrollerAddress = deployments['Unitroller'];
+  const tTokenCollateral = await ethers.getContractAt(
+    CERC20,
+    deployments[collateralToken.symbol]
+  );
+
+  const uTokenBorrow = await ethers.getContractAt(
+    IERC20,
+    borrowToken.underlying
+  );
+
+  const tTokenBorrow = await ethers.getContractAt(
+    CERC20,
+    deployments[borrowToken.symbol]
+  );
+
+  const comptrollerAddress = deployments["Unitroller"];
   const comptrollerAbi = await getAbiFromArbiscan(comptrollerAddress);
 
   const comptroller = new Contract(
@@ -55,79 +94,122 @@ const preLiquidationTest = async (deployments, collateralToken, borrowToken) => 
 
   let unitroller = new Contract(comptrollerAddress, unitrollerAbi, admin);
 
-  const mockPriceOracle = await ethers.getContractAt("MockPriceOracle", deployments['MockPriceOracle'])
+  const mockPriceOracle = await ethers.getContractAt(
+    "MockPriceOracle",
+    deployments["MockPriceOracle"]
+  );
 
-  for (let token of await unitroller.getAllMarkets()){
-    await mockPriceOracle.mockUpdatePrice(token, formatAmount('1', 18))
+  for (let token of await unitroller.getAllMarkets()) {
+    await mockPriceOracle.mockUpdatePrice(token, formatAmount("1", 18));
   }
 
-  await unitroller._setPriceOracle(mockPriceOracle.address)
+  await unitroller._setPriceOracle(mockPriceOracle.address);
 
-  await uTokenCollateral.connect(uCollateralWallet).approve(tTokenCollateral.address, ethers.constants.MaxUint256)
-  await uTokenBorrow.connect(uBorrowWallet).approve(tTokenBorrow.address, ethers.constants.MaxUint256)
+  await uTokenCollateral
+    .connect(uCollateralWallet)
+    .approve(tTokenCollateral.address, ethers.constants.MaxUint256);
+  await uTokenBorrow
+    .connect(uBorrowWallet)
+    .approve(tTokenBorrow.address, ethers.constants.MaxUint256);
 
-  await uTokenCollateral.connect(uCollateralWallet).transfer(uBorrowWallet.address, formatAmount('10', 6))
+  await uTokenCollateral
+    .connect(uCollateralWallet)
+    .transfer(uBorrowWallet.address, formatAmount("10", 6));
 
-  await tTokenCollateral.connect(uCollateralWallet).mint(formatAmount('2', 6));
-  await tTokenBorrow.connect(uBorrowWallet).mint(formatAmount('1000', 6));
+  await tTokenCollateral.connect(uCollateralWallet).mint(formatAmount("2", 6));
+  await tTokenBorrow.connect(uBorrowWallet).mint(formatAmount("1000", 6));
 
-  const uBalanceSupplied = await underlyingBalance(tTokenCollateral, uCollateralWallet)
-  const {1: collateralFactor} = await unitroller.markets(tTokenBorrow.address)
-  const maxBorrow = (uBalanceSupplied.mul(collateralFactor)).div(formatAmount('1', 18))
+  const uBalanceSupplied = await underlyingBalance(
+    tTokenCollateral,
+    uCollateralWallet
+  );
+  const { 1: collateralFactor } = await unitroller.markets(
+    tTokenBorrow.address
+  );
+  const maxBorrow = uBalanceSupplied
+    .mul(collateralFactor)
+    .div(formatAmount("1", 18));
 
   await tTokenBorrow.connect(uCollateralWallet).borrow(maxBorrow);
 
-
   await mockPriceOracle.mockUpdatePrice(
     tTokenCollateral.address,
-    formatAmount('.1', 18)
+    formatAmount(".1", 18)
   );
 
   const closeFactor = await unitroller.closeFactorMantissa();
-  const shortfall = await borrowedBalance(tTokenBorrow, uCollateralWallet)
-  const repayAmount = (shortfall.mul(closeFactor)).div(formatAmount('1', 16))
+  const shortfall = await borrowedBalance(tTokenBorrow, uCollateralWallet);
+  const repayAmount = shortfall.mul(closeFactor).div(formatAmount("1", 16));
 
-  const liquidatorBalancePre = await tTokenCollateral.balanceOf(uBorrowWallet.address)
-  const{2: preLiquidateShortfall} = await unitroller.getAccountLiquidity(uCollateralWallet.address)
-  await tTokenBorrow.connect(uBorrowWallet).liquidateBorrow(uCollateralWallet.address, repayAmount, tTokenCollateral.address)
-  const{2: postLiquidateShortfall} = await unitroller.getAccountLiquidity(uCollateralWallet.address)
-  const liquidatorBalancePost = await tTokenCollateral.balanceOf(uBorrowWallet.address)
+  const liquidatorBalancePre = await tTokenCollateral.balanceOf(
+    uBorrowWallet.address
+  );
+  const { 2: preLiquidateShortfall } = await unitroller.getAccountLiquidity(
+    uCollateralWallet.address
+  );
+  await tTokenBorrow
+    .connect(uBorrowWallet)
+    .liquidateBorrow(
+      uCollateralWallet.address,
+      repayAmount,
+      tTokenCollateral.address
+    );
+  const { 2: postLiquidateShortfall } = await unitroller.getAccountLiquidity(
+    uCollateralWallet.address
+  );
+  const liquidatorBalancePost = await tTokenCollateral.balanceOf(
+    uBorrowWallet.address
+  );
 
   return {
-    preLiquidation: { shortfall: preLiquidateShortfall, liquidatorBalance: liquidatorBalancePre },
-    postLiquidation: { shortfall: postLiquidateShortfall, liquidatorBalance: liquidatorBalancePost }
-  }
-}
+    preLiquidation: {
+      shortfall: preLiquidateShortfall,
+      liquidatorBalance: liquidatorBalancePre,
+    },
+    postLiquidation: {
+      shortfall: postLiquidateShortfall,
+      liquidatorBalance: liquidatorBalancePost,
+    },
+  };
+};
 
 const fundWithEth = async (receiver) => {
   const [ethWallet] = await ethers.getSigners();
-  await ethWallet.sendTransaction({to: receiver, value: ethers.utils.parseEther("1.0")})
-}
+  await ethWallet.sendTransaction({
+    to: receiver,
+    value: ethers.utils.parseEther("1.0"),
+  });
+};
 
 describe("Test", () => {
   let deployments;
-  const collateralToken = CTOKENS['tUSDC'];
-  const borrowToken = CTOKENS['tUSDT'];
+  const collateralToken = CTOKENS["tUSDC"];
+  const borrowToken = CTOKENS["tUSDT"];
 
   before(async () => {
     await resetNetwork();
-    await deploy('arbitrum');
-    deployments = getDeployments('localhost');
-  })
+    await deploy("arbitrum");
+    deployments = getDeployments("localhost");
+  });
   describe("Liquidation", () => {
     let preLiquidation;
     let postLiquidation;
     before(async () => {
-      let results = await preLiquidationTest(deployments, collateralToken, borrowToken);
+      let results = await preLiquidationTest(
+        deployments,
+        collateralToken,
+        borrowToken
+      );
       preLiquidation = results.preLiquidation;
       postLiquidation = results.postLiquidation;
-    })
+    });
     it("Borrower show have lower shortfall after liquidation", async () => {
-      expect(postLiquidation.shortfall).bignumber.lt(preLiquidation.shortfall)
-    })
+      expect(postLiquidation.shortfall).bignumber.lt(preLiquidation.shortfall);
+    });
     it("Liquidator should have more of liquidated token after liquidation", async () => {
-      expect(postLiquidation.liquidatorBalance).bignumber.gt(preLiquidation.liquidatorBalance)
-    })
-  })
-})
-
+      expect(postLiquidation.liquidatorBalance).bignumber.gt(
+        preLiquidation.liquidatorBalance
+      );
+    });
+  });
+});
--- tender/test/deploy/cdelegators.ts
+++ tender/test/deploy/cdelegators.ts
@@ -1,26 +1,30 @@
-import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-ethers";
 import hre, { ethers } from "hardhat";
 import { numToWei } from "../../utils/ethUnitParser";
 
 import { readFileSync, writeFileSync } from "fs";
-import {CTOKENS} from "./CTOKENS"
-import { resolve } from 'path';
+import { CTOKENS } from "./CTOKENS";
+import { resolve } from "path";
 
-const copyPath = resolve(__dirname, `../../../deployments/gmx.json`)
-const outputFilePath = resolve(__dirname, `../../../deployments/${hre.network.name}.json`)
+const copyPath = resolve(__dirname, `../../../deployments/gmx.json`);
+const outputFilePath = resolve(
+  __dirname,
+  `../../../deployments/${hre.network.name}.json`
+);
 
-const stakedGLPAddress = "0x2F546AD4eDD93B956C8999Be404cdCAFde3E89AE"
-const glpRewardRouterAddress = "0xA906F338CB21815cBc4Bc87ace9e68c87eF8d8F1"
-const glpManagerAddress = "0x321f653eed006ad1c29d174e17d96351bde22649"
+const stakedGLPAddress = "0x2F546AD4eDD93B956C8999Be404cdCAFde3E89AE";
+const glpRewardRouterAddress = "0xA906F338CB21815cBc4Bc87ace9e68c87eF8d8F1";
+const glpManagerAddress = "0x321f653eed006ad1c29d174e17d96351bde22649";
 
-
-export async function deploy (deploymentFp) {
-
+export async function deploy(deploymentFp) {
   const copyPath = deploymentFp
     ? resolve(__dirname, `../../../deployments/${deploymentFp}.json`)
-    : resolve(__dirname, `../../../deployments/gmx.json`)
+    : resolve(__dirname, `../../../deployments/gmx.json`);
 
-  const outputFilePath = resolve(__dirname, `../../../deployments/${hre.network.name}.json`)
+  const outputFilePath = resolve(
+    __dirname,
+    `../../../deployments/${hre.network.name}.json`
+  );
   const [deployer] = await hre.ethers.getSigners();
   console.log(`>>>>>>>>>>>> Deployer: ${deployer.address} <<<<<<<<<<<<\n`);
 
@@ -30,10 +34,10 @@ export async function deploy (deploymentFp) {
 
   const unitrollerProxy = await hre.ethers.getContractAt(
     "Comptroller",
-    unitrollerAddress,
+    unitrollerAddress
   );
 
-  const adminAddress = await unitrollerProxy.admin()
+  const adminAddress = await unitrollerProxy.admin();
   const admin = await ethers.getImpersonatedSigner(adminAddress);
 
   const delegatorFactory = await hre.ethers.getContractFactory(
@@ -43,7 +47,6 @@ export async function deploy (deploymentFp) {
 
   for (let i = 0; i < CTOKENS.length; i++) {
     let token = CTOKENS[i];
-    console.log(token)
 
     const erc20Underlying = await hre.ethers.getContractAt(
       "EIP20Interface",
@@ -53,65 +56,58 @@ export async function deploy (deploymentFp) {
     const totalDecimals = underlyingDecimals + token.decimals;
     const initialExcRateMantissaStr = numToWei("2", totalDecimals);
 
-    console.log("deploying delegate")
-    const deployedCErc20Delegate = await CErc20Delegate.deploy()
-    const delegateAddress = deployedCErc20Delegate.address
-
-    console.log("deployed CErc20Delegate", delegateAddress)
-
-    console.log(
-        "Calling delegatorFactory.deploy() with",
-        token.underlying,
-        unitrollerAddress,
-        irModelAddress,
-        initialExcRateMantissaStr,
-        token.name,
-        token.symbol,
-        token.decimals,
-        token.isGLP === true,
-        adminAddress,
-        delegateAddress,
-        Buffer.from([0x0]),
-    )
+    const deployedCErc20Delegate = await CErc20Delegate.deploy();
+    const delegateAddress = deployedCErc20Delegate.address;
 
     const delegator = await delegatorFactory.deploy(
-        token.underlying,
-        unitrollerAddress,
-        irModelAddress,
-        initialExcRateMantissaStr,
-        token.name,
-        token.symbol,
-        token.decimals,
-        token.isGLP === true,
-        adminAddress,
-        delegateAddress,
-        Buffer.from([0x0]),
+      token.underlying,
+      unitrollerAddress,
+      irModelAddress,
+      initialExcRateMantissaStr,
+      token.name,
+      token.symbol,
+      token.decimals,
+      token.isGLP === true,
+      adminAddress,
+      delegateAddress,
+      Buffer.from([0x0])
     );
 
     await delegator.deployed();
-    console.log("delegator deployed to:", delegator.address);
 
     if (token.isGLP) {
-      console.log("calling ctoken._setGlpAddresses()");
-      await delegator._setGlpAddresses(stakedGLPAddress, glpRewardRouterAddress, glpManagerAddress);
+      await delegator._setGlpAddresses(
+        stakedGLPAddress,
+        glpRewardRouterAddress,
+        glpManagerAddress
+      );
     }
 
-    console.log("calling unitrollerProxy._supportMarket()");
+    let isPrivate = token.isGLP === true;
+    let isComped = true;
+    let onlyWhitelistedBorrow = false;
+    await unitrollerProxy
+      .connect(admin)
+      ._supportMarket(
+        delegator.address,
+        isComped,
+        isPrivate,
+        onlyWhitelistedBorrow
+      );
 
-    let isPrivate = token.isGLP === true
-    let isComped = true
-    let onlyWhitelistedBorrow = false
-    await unitrollerProxy.connect(admin)._supportMarket(delegator.address, isComped, isPrivate, onlyWhitelistedBorrow);
-
-    console.log("calling unitrollerProxy._setCollateralFactor()")
-
-    await unitrollerProxy.connect(admin)._setFactorsAndThresholds(
-      delegator.address, token.collateralFactor, token.collateralVIP, token.threshold, token.thresholdVIP);
+    await unitrollerProxy
+      .connect(admin)
+      ._setFactorsAndThresholds(
+        delegator.address,
+        token.collateralFactor,
+        token.collateralVIP,
+        token.threshold,
+        token.thresholdVIP
+      );
 
     // Save to output
     deployments[`${token.symbol}_delegate`] = delegateAddress;
     deployments[token.symbol] = delegator.address;
     writeFileSync(outputFilePath, JSON.stringify(deployments, null, 2));
   }
-};
-
+}
