64c8e90b0f8905cb29818e721d8baa0137bd3c00
frehml <frederic.hamelink@hotmail.com>
2022-11-07 22:39:04 +0100

    revert erc20 test

--- tender/test/Erc20.ts
+++ tender/test/Erc20.ts
@@ -11,42 +11,50 @@ import * as hre from "hardhat";
 import * as ethers from "ethers";
 import { Contract, BigNumber } from "ethers";
 import { expect } from "chai";
-import { formatAmount } from "./utils/TokenUtil";
-import { resolve } from 'path'
-import { parseAbiFromJson, getDeployments } from "./utils/TestUtil";
+import { formatAmount, getUnderlyingBalance } from "./utils/TokenUtil";
 import * as tokenClasses from "./contract_helpers/Token";
 const hreProvider = hre.network.provider;
 
 const provider = new ethers.providers.Web3Provider(hreProvider as any);
 // USE THE DELEGATOR FILE INSTEAD OF THE CETHER AND CERC20!!!!
 
-const test = {
-  symbol: "tUSDC",
-  contractName: "CErc20",
-  mintAmount: "4",
-  borrowAmount: "1",
-  redeemAmount: "1",
-  redeemUnderlyingAmount: "1",
-  contractClass: CTokenContract,
-  liquidate: "yes",
-  address: "0xB1087a450373BB26BCf1A18E788269bde9c8fc85",
-};
+const tests = [
+  {
+    symbol: "tUSDC",
+    contractName: "CErc20",
+    mintAmount: "4",
+    borrowAmount: "1",
+    redeemAmount: "1",
+    redeemUnderlyingAmount: "1",
+    contractClass: CTokenContract,
+    liquidate: "yes",
+  },
+  //{
+  //  symbol: "tEth",
+  //  contractName: "CEther",
+  //  mintAmount: "0.001",
+  //  redeemAmount: "all",
+  //  borrowAmount: "0.0005",
+  //  contractClass: CTokenContract,
+  //},
 
-const testUsdt = {
-  symbol: "tUSDT",
-  contractName: "CErc20",
-  mintAmount: "4",
-  borrowAmount: "1",
-  redeemAmount: "1",
-  redeemUnderlyingAmount: "1",
-  contractClass: CTokenContract,
-  liquidate: "yes",
-  address: "0x102517Ea9340eDd21afdfAA911560311FeEFc607",
-};
+  // {
+  //   symbol: 'tGMX',
+  //   contractName: 'CErc20DelegatorGmx',
+  //   mintAmount: '0.01',
+  //   borrowAmount: '1',
+  //   redeemAmount: '1',
+  //   redeemUnderlyingAmount: '1',
+  //   contractClass: GmxTokenContract,
+  //   deploymentFilePath: '../../deployments/gmx.json',
+  //   walletAddress: '0x5B33EC561Cb20EaF7d5b41A9B68A690E2EBBc893',
+  // }
+];
 
 const testOptionalDefaults = {
   contractClass: CTokenContract,
   deploymentFilePath: "../../deployments/arbitrum.json",
+  walletAddress: "0x52134afB1A391fcEEE6682E51aedbCD47dC55336",
 };
 
 const verifyTestParameters = (test) => {
@@ -56,18 +64,16 @@ const verifyTestParameters = (test) => {
   return test;
 };
 
-let tUsdcContract: CTokenContract;
-let tUsdtContract: CTokenContract;
+let erc20Contract: CTokenContract;
 let comptrollerContract: ComptrollerContract;
 let uContractAddress: string;
 let uContract: Contract;
-let wallet;
+let wallet: JsonRpcSigner;
 let admin: JsonRpcSigner;
-let usdtContract;
 
 let uBalanceProvider: Contract | JsonRpcProvider;
 
-let walletAddress = "0x52134afB1A391fcEEE6682E51aedbCD47dC55336";
+const walletAddress = "0x52134afB1A391fcEEE6682E51aedbCD47dC55336";
 const adminAddress = "0x85abbc0f8681c4fb33b6a3a601ad99e92a32d1ac";
 
 describe("Erc20", () => {
@@ -75,144 +81,137 @@ describe("Erc20", () => {
     resetNetwork();
     wallet = await getWallet(walletAddress, provider);
     admin = await getWallet(adminAddress, provider);
-    tUsdcContract = new CTokenContract(
-      test["symbol"],
-      test["contractName"],
-      wallet,
-      test["deploymentFile"]
-    );
-
-    const uAbiPath = resolve(
-      __dirname,
-      `../../artifacts/contracts/CErc20.sol/CErc20.json`
-    );
-    const uAbi = parseAbiFromJson(uAbiPath);
-
-    let uContractOne = new Contract(await tUsdcContract.underlying(), uAbi, wallet);
-
-    tUsdcContract.address = test["address"];
-    await uContractOne.approve(
-      tUsdcContract.address,
-      '1000000000000000000000000000'
-    );
-
-
-    tUsdtContract = new CTokenContract(
-      testUsdt["symbol"],
-      testUsdt["contractName"],
-      wallet,
-      testUsdt["deploymentFile"]
-    );
-    tUsdtContract.address = testUsdt["address"];
-    let uContractTwo = new Contract(await tUsdtContract.underlying(), uAbi, wallet);
-    usdtContract = uContractTwo;
-
-
-    await uContractTwo.approve(
-      tUsdtContract.address,
-      '1000000000000000000000000000'
-    );
   });
-  describe("Liquidate", () => {
-    let oracleContract: OracleContract;
-    let liquidator: JsonRpcSigner;
-    let unitroller;
-    const liquidatorAddress = "0x88964546aa6d9da1306bf0a29cc01cde8b0c660e";
 
-    before(async () => {
-      const comptrollerAddress = await tUsdtContract.comptroller();
-      const comptrollerAbi = await getAbiFromArbiscan(comptrollerAddress);
-      const comptroller = new Contract(
-        comptrollerAddress,
-        comptrollerAbi,
-        wallet
-      );
-
-      const unitrollerAddress = await comptroller.comptrollerImplementation();
-      const unitrollerAbi = await getAbiFromArbiscan(unitrollerAddress);
-
-      unitroller = new Contract(
-        await comptrollerAddress,
-        unitrollerAbi,
-        admin
-      );
-
-      liquidator = await getWallet(liquidatorAddress, provider);
-      oracleContract = new OracleContract("MockPriceOracle", wallet);
-      await unitroller._setPriceOracle(oracleContract.address);
-      await unitroller.connect(wallet);
-    });
-
-    it("Should liquidate if account liquidity is negative", async () => {
-
-      await oracleContract.mockUpdatePrice(
-        tUsdcContract.address,
-        formatAmount('1', 18)
-      );
-
-      await oracleContract.mockUpdatePrice(
-        tUsdtContract.address,
-        formatAmount('1', 18)
-      );
-
-      await oracleContract.mockUpdatePrice(
-        "0x593b3eF799b219d80dD8F0556d1aA8bC362fe48C",
-        formatAmount('1', 18)
-      )
-
-      let tEthContract = new CTokenContract('tEth', 'CEther', wallet);
-
-      console.log("Before Everything",
-        await unitroller.getAccountLiquidity(walletAddress)
-      );
-
-      await unitroller.enterMarkets([tUsdcContract.address, tUsdtContract.address, tEthContract.address]);
-
-
-      console.log("Before Everything",
-        await unitroller.getAccountLiquidity(walletAddress)
-      );
-
-      await tEthContract.redeem(await tEthContract.balanceOf(walletAddress))
-      let assets = await unitroller.getAssetsIn(walletAddress);
-
-      let usdtWallet = await getWallet('0x9bf54297d9270730192a83EF583fF703599D9F18', provider)
-
-      // error code
-      // account liquidity
-      console.log("Before Everything",
-        await unitroller.getAccountLiquidity(walletAddress)
-      );
-      await usdtContract.connect(usdtWallet).approve(tUsdtContract.address, '1000000000000000000000000000000')
-      await tUsdtContract.contract.connect(usdtWallet).mint(1000)
-
-      let usdcBalance = await tUsdcContract.getUnderlyingBalance(walletAddress);
-
-      console.log(usdcBalance)
-      await tUsdcContract.contract.mint(usdcBalance);
-      console.log(await tUsdcContract.balanceOf(walletAddress))
-
-      assets = await unitroller.getAssetsIn(walletAddress);
-      console.log(assets)
-
-      console.log("After Supply Eth",
-        await unitroller.getAccountLiquidity(walletAddress)
-      );
-
-      await tUsdtContract.borrow(await tUsdcContract.balanceOf(walletAddress));
-
-      console.log("After Borrow",
-        await unitroller.getAccountLiquidity(walletAddress)
-      );
-
-      await oracleContract.mockUpdatePrice(
-        tUsdtContract.address,
-        formatAmount('3', 18)
-      );
-
-      console.log("After Price Update",
-        await unitroller.getAccountLiquidity(walletAddress)
-      );
-    });
+  after(async () => {
+    resetNetwork();
   });
+
+  for (let test of tests) {
+    describe(test.symbol, () => {
+      before(async () => {
+        test = verifyTestParameters(test);
+        // if(wallet._address != test.walletAddress) {
+        //   console.log('impersonating: ', test.walletAddress);
+        //   wallet = await getWallet(test.walletAddress, provider);
+        // }
+        erc20Contract = new test["contractClass"](
+          test.symbol,
+          test.contractName,
+          wallet
+        );
+        comptrollerContract = new ComptrollerContract(admin);
+      });
+      if (test["mintAmount"]) {
+        describe("Mint", () => {
+          it("Should have more tTokens and fewer uTokens", async () => {
+            const tBalance = await erc20Contract.balanceOf(wallet._address);
+            const uBalance = await erc20Contract.getUnderlyingBalance(
+              wallet._address
+            );
+
+            await erc20Contract.supply(test.mintAmount);
+
+            const tBalanceTest = (
+              await erc20Contract.balanceOf(wallet._address)
+            )
+              .sub(tBalance)
+              .gt(0);
+            const uBalanceTest = (
+              await erc20Contract.getUnderlyingBalance(wallet._address)
+            )
+              .sub(uBalance)
+              .lt(0);
+
+            expect(tBalanceTest).to.be.true;
+            expect(uBalanceTest).to.be.true;
+          });
+        });
+      }
+      if (test["redeemAmount"]) {
+        describe("redeem", () => {
+          it("Should have less tTokens and more uTokens", async () => {
+            let redeemAmount;
+
+            const tBalance = await erc20Contract.balanceOf(wallet._address);
+            const uBalance = await erc20Contract.getUnderlyingBalance(
+              wallet._address
+            );
+
+            if (test["redeemAmount"] == "all") {
+              redeemAmount = tBalance;
+            } else {
+              redeemAmount = formatAmount(
+                test["redeemAmount"],
+                await erc20Contract.decimals()
+              );
+            }
+
+            await erc20Contract.approve(wallet._address, redeemAmount);
+            await erc20Contract.redeem(redeemAmount);
+
+            const tBalanceTest = (
+              await erc20Contract.balanceOf(wallet._address)
+            )
+              .sub(tBalance)
+              .lt(0);
+            const uBalanceTest = (
+              await erc20Contract.getUnderlyingBalance(wallet._address)
+            )
+              .sub(uBalance)
+              .gt(0);
+
+            expect(tBalanceTest).to.be.true;
+            expect(uBalanceTest).to.be.true;
+          });
+        });
+      }
+      if (test["redeemUnderlyingAmount"]) {
+        describe("redeemUnderlying", () => {
+          it("Should have less tTokens and more uTokens", async () => {
+            const tBalance = await erc20Contract.balanceOf(wallet._address);
+            const uBalance = await erc20Contract.getUnderlyingBalance(
+              wallet._address
+            );
+
+            const redeemUnderlyingAmount = formatAmount(
+              test["redeemUnderlyingAmount"],
+              erc20Contract.uDecimals
+            );
+
+            await erc20Contract.approve(
+              wallet._address,
+              redeemUnderlyingAmount
+            );
+            await erc20Contract.redeem(redeemUnderlyingAmount);
+
+            const tBalanceTest = (
+              await erc20Contract.balanceOf(wallet._address)
+            )
+              .sub(tBalance)
+              .lt(0);
+            const uBalanceTest = (
+              await erc20Contract.getUnderlyingBalance(wallet._address)
+            )
+              .sub(uBalance)
+              .gt(0);
+
+            expect(tBalanceTest).to.be.true;
+            expect(uBalanceTest).to.be.true;
+          });
+        });
+      }
+      if (test["borrowAmount"]) {
+        describe("Borrow", () => {
+          // it('Should have more tTokens and fewer uTokens', async () => {
+          //   let borrowBalance = await erc20Contract.borrowBalanceStored()
+          //   console.log(borrowBalance.toString())
+          //   await erc20Contract.borrow(formatAmount(.0005));
+          //   borrowBalance = await erc20Contract.borrowBalanceStored()
+          //   console.log(borrowBalance.toString())
+          // });
+        });
+      }
+    });
+  }
 });
