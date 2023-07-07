3ec20c1b962215e9ed7fb4e30572fb502adeec95
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-07-07 16:43:47 -0400

    Set up zkSync testnet and local test config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,45 @@
+import 'dotenv/config';
+
+import { HardhatUserConfig } from 'hardhat/config';
+import { Wallet, Provider } from "zksync-web3";
+
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 import "@matterlabs/hardhat-zksync-verify";
 
-module.exports = {
+/* note: boolean environment variables are imported as strings */
+const { ETH_PK = "", } = process.env;
+
+export function requireEnv(varName, msg?: string): string {
+  const varVal = process.env[varName];
+  if (!varVal) {
+    throw new Error(msg ?? `Missing required environment variable "${varName}"`);
+  }
+  return varVal;
+}
+
+[ "ETH_PK" ].map(v => requireEnv(v));
+
+
+const config: HardhatUserConfig = {
   zksolc: {
     version: "latest", // Uses latest available in https://github.com/matter-labs/zksolc-bin/
     settings: {},
   },
 
-  defaultNetwork: "zkSyncTestnet",
+  defaultNetwork: "zkSyncLocal",
 
   networks: {
+    zkSyncLocal: {
+      url: "http://localhost:3050",
+      ethNetwork: "http://localhost:8545",
+      zksync: true,
+    },
     zkSyncTestnet: {
       url: "https://testnet.era.zksync.dev",
       ethNetwork: "goerli", // RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
       zksync: true,
-      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'  // Verification endpoint
+      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification"  // Verification endpoint
     },
   },
 
@@ -23,3 +47,10 @@ module.exports = {
     version: "0.8.10",
   },
 };
+
+extendEnvironment((hre) => {
+  const wallet = new Wallet(ETH_PK);
+  hre.zkWallet = wallet;
+});
+
+module.exports = config;
