ab33c514ba481cb18db0681412ec3b70c9d875f3
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-07-08 20:12:38 -0400

    Test borrow on zkSync

--- script/zksync/test.ts
+++ script/zksync/test.ts
@@ -20,13 +20,19 @@ async function main() {
   const testUsdBalance = await testUsd.balanceOf(wallet.address);
   logBalance("TEST", testUsdBalance);
 
-
   const ctUsdAddress = "0xcFDE18a0f130bBAfe0037072407F83899D49414f";
   const ctUsd = await hre.ethers.getContractAt(
     "CErc20Immutable",
     ctUsdAddress,
     wallet
   );
+
+  const priceOracleAddress = "0x1F0151386fB0AbBF0273238dF5E9bc519DE5e20B";
+  const priceOracle = await hre.ethers.getContractAt("SimplePriceOracle", priceOracleAddress, wallet);
+
+  const price = utils.parseEther("1");
+  console.log(`Setting price oracle to ${utils.formatEther(price)}...`);
+  await priceOracle.setUnderlyingPrice(ctUsd.address, price);
 
   const mintAmount = utils.parseEther("1");
 
@@ -40,8 +46,34 @@ async function main() {
   logBalance("zTEST", ctUsdBalance);
 
   const comptrollerAddress = "0x5B11c36bf87ED2EAc102C42E9528eC99D77f7aFd";
+  const comptroller = await hre.ethers.getContractAt("Comptroller", comptrollerAddress, wallet);
+
+  const ctokens = {
+      "zTEST": ctUsd.address,
+  };
+
+  console.log(`Entering the ${Object.keys(ctokens).join(", ")} markets...`);
+  await comptroller.enterMarkets(Object.values(ctokens));
+
+  const collateralFactor:BigNumber = utils.parseEther("0.5");
+
+  console.log(`Setting collateral factor to ${utils.formatEther(collateralFactor)}...`);
+  const receipt = await comptroller._setCollateralFactor(ctUsd.address, collateralFactor);
+
+  const { isListed, collateralFactorMantissa, isComped } = await comptroller.markets(ctUsd.address);
+  console.log(`TEST market is ${isListed ? "" : "not"} listed with a collateral factor of ${utils.formatEther(collateralFactorMantissa)}`);
+
+  const accountLiquidity = await comptroller.getAccountLiquidity(wallet.address);
+  console.log(`Account liquidity: ${accountLiquidity.toString()}`);
+
+  const borrowAmount = utils.parseEther("0.25");
+
+  console.log(`Borrowing ${utils.formatEther(borrowAmount)} TEST...`);
+  await ctUsd.borrow(borrowAmount);
+
+  logBalance("TEST", testUsdBalance);
+
   const interestRateModelAddress = "0x29c6fF2E3D04a9f37e7af1fF9b38C9E2e9079FfA";
-  const priceOracleAddress = "0x1F0151386fB0AbBF0273238dF5E9bc519DE5e20B";
 }
 
 main().catch((error) => {
